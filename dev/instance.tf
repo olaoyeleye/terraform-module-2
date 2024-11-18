068b1419879084d6d8401a8b30ee499310dbd990
Olu <olaoyeleye@yahoo.co.uk>
2024-11-18 19:14:34 +0000

    second commit

--- dev/instance.tf
+++ dev/instance.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "node_sg" {
   name        = "week9_node_security"
-  vpc_id      = "vpc-0dec0ac0d693b26e7"
+  vpc_id      = "vpc-0e26d767ace1ef817"
 
   tags = {
     Name = "week9_node_security"
@@ -22,7 +22,7 @@ resource "aws_security_group" "node_sg" {
 resource "aws_instance" "node1" {
     ami  = "ami-0b4c7755cdf0d9219"
     instance_type = "t2.micro"
-    key_name ="hello123"
+    key_name ="kensko-2"
 
     security_groups =  [aws_security_group.node_sg.name]
 
@@ -35,7 +35,7 @@ resource "aws_instance" "node1" {
 resource "aws_instance" "node2" {
     ami  = "ami-0b4c7755cdf0d9219"
     instance_type = "t2.micro"
-    key_name ="hello123"
+    key_name ="kensko-2"
 
     tags = {
         Name  = var.node2
