995fcb4d2df186387b0d3ebdd9a40b712e6c49aa
Olu <olaoyeleye@yahoo.co.uk>
2024-12-05 23:31:07 +0000

    i have updated the SG for python node to only allow nginx ip

--- dev/output.tf
+++ dev/output.tf
@@ -1,6 +1,9 @@
 output "nginx" {
     value =aws_instance.node1.public_dns
 }
+output "nginx-private" {
+    value =aws_instance.node1.private_dns
+}
 
 output "python-1" {
     value =aws_instance.node2.public_dns
