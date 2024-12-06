5405d3305f4f95412c12cd2604222d3093959f00
Olu <olaoyeleye@yahoo.co.uk>
2024-12-06 01:30:33 +0000

    added depends on both nginx node and nginx SG

--- dev/instance.tf
+++ dev/instance.tf
@@ -49,8 +49,7 @@ resource "aws_security_group" "week10_python_node_sg" {
     from_port = 65432
     to_port = 65432
     protocol = "tcp" 
-    security_groups = [aws_security_group.week10_nginx_node_sg.id]
-    #cidr_blocks =  ["${aws_instance.node1.public_ip}/32", "${aws_instance.node1.public_ip}/32", "${aws_instance.node1.public_ip}/32"] 
+    security_groups = ["${aws_security_group.week10_nginx_node_sg.id}"]
     #cidr_blocks =  ["${aws_instance.node1.private_ip}/32"]  #["0.0.0.0/0"]
   }
   egress  {
